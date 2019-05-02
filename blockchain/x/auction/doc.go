dd2ec57c3a7889238433ce03eea64312bbe42bc2
Ruaridh <rhuairahrighairidh@users.noreply.github.com>
2019-05-01 20:56:15 -0400

    add TODOs

--- blockchain/x/auction/doc.go
+++ blockchain/x/auction/doc.go
@@ -2,9 +2,10 @@
 Package auction is a module for creating generic auctions and allowing users to place bids until a timeout is reached.
 
 TODO
- - add more test cases
+ - add more test cases, add stronger validation to user inputs
  - add minimum bid increment
  - decided whether to put auction params like default timeouts into the auctions themselves
+ - add docs
  - user facing things like cli, rest, querier, tags
  - custom error types, codespace
 */
