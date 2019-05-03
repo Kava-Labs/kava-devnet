68a62c962613d54a716c435560aab5cedcd477a9
Kevin Davis <kjydavis3@gmail.com>
2019-05-03 12:39:31 -0400

    feat: keeper and msg tests

--- blockchain/x/pricefeed/errors.go
+++ blockchain/x/pricefeed/errors.go
@@ -12,9 +12,23 @@ const (
 
 	// CodeEmptyInput error code for empty input errors
 	CodeEmptyInput sdk.CodeType = 1
+	// CodeExpired error code for expired prices
+	CodeExpired sdk.CodeType = 2
+	// CodeInvalid error code for all input prices expired
+	CodeInvalid sdk.CodeType = 3
 )
 
 // ErrEmptyInput Error constructor
 func ErrEmptyInput(codespace sdk.CodespaceType) sdk.Error {
 	return sdk.NewError(codespace, CodeEmptyInput, fmt.Sprintf("Input must not be empty."))
+}
+
+// ErrExpired Error constructor for posted price messages with expired price
+func ErrExpired(codespace sdk.CodespaceType) sdk.Error {
+	return sdk.NewError(codespace, CodeExpired, fmt.Sprintf("Price is expired."))
+}
+
+// ErrNoValidPrice Error constructor for posted price messages with expired price
+func ErrNoValidPrice(codespace sdk.CodespaceType) sdk.Error {
+	return sdk.NewError(codespace, CodeInvalid, fmt.Sprintf("All input prices are expired."))
 }
