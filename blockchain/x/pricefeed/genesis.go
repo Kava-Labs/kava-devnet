bb3bcbcbc8d8e80f8b7357cc0180602268d2d7a7
Kevin Davis <kjydavis3@gmail.com>
2019-08-15 12:27:17 -0400

    feat: implement module spec

--- blockchain/x/pricefeed/genesis.go
+++ blockchain/x/pricefeed/genesis.go
@@ -1,40 +1,51 @@
 package pricefeed
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
 
-// GenesisState state at gensis
-type GenesisState struct {
-	Assets  []Asset
-	Oracles []Oracle
-}
 
 // InitGenesis sets distribution information for genesis.
-func InitGenesis(ctx sdk.Context, keeper Keeper, genState GenesisState) {
-	for _, asset := range genState.Assets {
-		keeper.AddAsset(ctx, asset.AssetCode, asset.Description)
+func InitGenesis(ctx sdk.Context, keeper Keeper, data GenesisState) {
+
+	// Set the assets and oracles from params
+	keeper.SetAssetParams(ctx, data.AssetParams)
+	keeper.SetOracleParams(ctx ,data.OracleParams)
+
+	// Iterate through the posted prices and set them in the store
+	for _, pp := range data.PostedPrices {
+		addr, err := sdk.AccAddressFromBech32(pp.OracleAddress)
+		if err != nil {
+			panic(err)
+		}
+		_, err = keeper.SetPrice(ctx, addr, pp.AssetCode, pp.Price, pp.Expiry)
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	for _, oracle := range genState.Oracles {
-		keeper.AddOracle(ctx, oracle.OracleAddress)
+	// Set the current price (if any) based on what's now in the store
+	if err := keeper.SetCurrentPrices(ctx); err != nil {
+		panic(err)
 	}
-}
-
-// DefaultGenesisState returns a default genesis state
-func DefaultGenesisState() GenesisState {
-	return GenesisState{
-		[]Asset{{"btc", "a description"}, {"xrp", "the standard"}},
-		[]Oracle{}}
-}
-
-// ValidateGenesis performs basic validation of genesis data returning an
-// error for any failed validation criteria.
-func ValidateGenesis(data GenesisState) error {
-	// TODO
-	return nil
 }
 
 // ExportGenesis returns a GenesisState for a given context and keeper.
 func ExportGenesis(ctx sdk.Context, keeper Keeper) GenesisState {
-	// TODO implement this
-	return DefaultGenesisState()
+
+	// Get the params for assets and oracles
+	assetParams := keeper.GetAssetParams(ctx)
+	oracleParams := keeper.GetOracleParams(ctx)
+
+	var postedPrices []PostedPrice
+	for _, asset := range keeper.GetAssets(ctx) {
+		pp := keeper.GetRawPrices(ctx, asset.AssetCode)
+		postedPrices = append(postedPrices, pp...)
+	}
+
+	return GenesisState{
+		AssetParams:  assetParams,
+		OracleParams: oracleParams,
+		PostedPrices: postedPrices,
+	}
 }
--- blockchain/x/pricefeed/module.go
+++ blockchain/x/pricefeed/module.go
@@ -3,20 +3,25 @@ package pricefeed
 import (
 	"encoding/json"
 
+	"github.com/gorilla/mux"
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/kava-labs/kava-devnet/blockchain/x/pricefeed/client/cli"
+	"github.com/kava-labs/kava-devnet/blockchain/x/pricefeed/client/rest"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 var (
-	_ sdk.AppModule      = AppModule{}
-	_ sdk.AppModuleBasic = AppModuleBasic{}
+	_ module.AppModule      = AppModule{}
+	_ module.AppModuleBasic = AppModuleBasic{}
 )
-
 // AppModuleBasic app module basics object
 type AppModuleBasic struct{}
-
-var _ sdk.AppModuleBasic = AppModuleBasic{}
 
 // Name get module name
 func (AppModuleBasic) Name() string {
@@ -30,17 +35,32 @@ func (AppModuleBasic) RegisterCodec(cdc *codec.Codec) {
 
 // DefaultGenesis default genesis state
 func (AppModuleBasic) DefaultGenesis() json.RawMessage {
-	return moduleCdc.MustMarshalJSON(DefaultGenesisState())
+	return ModuleCdc.MustMarshalJSON(DefaultGenesisState())
 }
 
 // ValidateGenesis module validate genesis
 func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 	var data GenesisState
-	err := moduleCdc.UnmarshalJSON(bz, &data)
+	err := ModuleCdc.UnmarshalJSON(bz, &data)
 	if err != nil {
 		return err
 	}
 	return ValidateGenesis(data)
+}
+
+// RegisterRESTRoutes register rest routes
+func (AppModuleBasic) RegisterRESTRoutes(ctx context.CLIContext, rtr *mux.Router) {
+	rest.RegisterRoutes(ctx, rtr, StoreKey)
+}
+
+// GetTxCmd get the root tx command of this module
+func (AppModuleBasic) GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	return cli.GetTxCmd(StoreKey, cdc)
+}
+
+// GetQueryCmd get the root query command of this module
+func (AppModuleBasic) GetQueryCmd(cdc *codec.Codec) *cobra.Command {
+	return cli.GetQueryCmd(StoreKey, cdc)
 }
 
 // AppModule app module type
@@ -63,7 +83,7 @@ func (AppModule) Name() string {
 }
 
 // RegisterInvariants register module invariants
-func (AppModule) RegisterInvariants(_ sdk.InvariantRouter) {}
+func (AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 
 // Route module message route name
 func (AppModule) Route() string {
@@ -85,28 +105,25 @@ func (am AppModule) NewQuerierHandler() sdk.Querier {
 	return NewQuerier(am.keeper)
 }
 
+// BeginBlock module begin-block
+func (AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
+
+// EndBlock module end-block
+func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+	EndBlocker(ctx, am.keeper)
+	return []abci.ValidatorUpdate{}
+}
+
 // InitGenesis module init-genesis
 func (am AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.ValidatorUpdate {
 	var genesisState GenesisState
-	moduleCdc.MustUnmarshalJSON(data, &genesisState)
+	ModuleCdc.MustUnmarshalJSON(data, &genesisState)
 	InitGenesis(ctx, am.keeper, genesisState)
-
 	return []abci.ValidatorUpdate{}
 }
 
 // ExportGenesis module export genesis
 func (am AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
 	gs := ExportGenesis(ctx, am.keeper)
-	return moduleCdc.MustMarshalJSON(gs)
-}
-
-// BeginBlock module begin-block
-func (AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) sdk.Tags {
-	return sdk.EmptyTags()
-}
-
-// EndBlock module end-block
-func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) ([]abci.ValidatorUpdate, sdk.Tags) {
-	EndBlocker(ctx, am.keeper)
-	return []abci.ValidatorUpdate{}, sdk.Tags{}
+	return ModuleCdc.MustMarshalJSON(gs)
 }
